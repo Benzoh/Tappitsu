618b573139135be1459270770f4f3afd7f3323af
Benzoh Wada <benzoh.wada@gmail.com>
2016-05-17 23:38:33 +0900

    Update base.php
    アドセンス除去

--- view/base.php
+++ view/base.php
@@ -15,16 +15,6 @@
 	<link rel="stylesheet" href="../_assets/css/font_style.css" media="screen">
 </head>
 <body>
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-42430007-3', 'auto');
-	  ga('send', 'pageview');
-
-	</script>
 	<div class="wrap">
 
 		<header>
@@ -56,15 +46,7 @@
 
 		<div class="ads">
 	
-			<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-			<!-- 達筆で使用 -->
-			<ins class="adsbygoogle"
-			     style="display:inline-block;width:728px;height:90px"
-			     data-ad-client="ca-pub-7815961604338808"
-			     data-ad-slot="9150074171"></ins>
-			<script>
-			(adsbygoogle = window.adsbygoogle || []).push({});
-			</script>
+
 
 		</div>
 
@@ -72,15 +54,7 @@
 		
 		<div class="ads">
 		
-			<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-			<!-- 達筆 スマホで使用 -->
-			<ins class="adsbygoogle"
-			     style="display:inline-block;width:300px;height:250px"
-			     data-ad-client="ca-pub-7815961604338808"
-			     data-ad-slot="1487206579"></ins>
-			<script>
-			(adsbygoogle = window.adsbygoogle || []).push({});
-			</script>
+
 
 		</div>
 
